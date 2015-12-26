867449cb40685606873e0d9743a6291cca49751f
Valeriy.Vyrva <valery1707@gmail.com>
2015-12-26 22:13:25 +0600

    Badge: redirect without actual version detect

---
+++ src/main/java/name/valery1707/javadocBadge/doc/BadgeController.java
@@ -0,0 +1,61 @@
+package name.valery1707.javadocBadge.doc;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("doc")
+public class BadgeController {
+	@Value("${badge.javadocURL}")
+	private String javadocURL;
+
+	@Value("${badge.shieldsURL}")
+	private String shieldsBaseURL;
+
+	@Value("${badge.defaultColor}")
+	private String badgeColor;
+
+	@RequestMapping(value = "{groupId}/{artifactId}/badge.{ext}", method = RequestMethod.GET)
+	//todo version?
+	//todo HTTP/HTTPS?
+	public ResponseEntity<String> badge(
+			@PathVariable("groupId") String groupId,
+			@PathVariable("artifactId") String artifactId,
+			@PathVariable("ext") String ext,
+			@RequestParam("style") Optional<String> style,
+			@RequestParam("color") Optional<String> color
+	) {
+		String version = "0.0.0-SNAPSHOT";//todo Detect actual version
+		UriComponentsBuilder shieldURIBuilder = ServletUriComponentsBuilder.fromHttpUrl(shieldsBaseURL);
+		shieldURIBuilder.path("{subject}-{version}-{color}.{ext}");
+		if (style.isPresent()) {
+			shieldURIBuilder.queryParam("style", style.orElse(null));
+		}
+		Map<String, String> params = new HashMap<>(4);
+		params.put("subject", "javadoc");
+		params.put("version", badgePart(version));
+		params.put("color", badgePart(color.orElse(badgeColor)));
+		params.put("ext", ext);
+		UriComponents shieldURI = shieldURIBuilder.build().expand(params);
+		return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT)
+				.header("Location", shieldURI.toUriString())
+				.body(null);
+	}
+
+	private static String badgePart(String src) {
+		return src.replace("-", "--");
+	}
+}
