2a4397200d966a8234efa8c2c35ee8156eb67d50
Valeriy.Vyrva <valery1707@gmail.com>
2015-12-26 17:48:17 +0600

    Status: detect application version

--- src/main/java/name/valery1707/testSpringRedirect/status/StatusRepository.java
+++ src/main/java/name/valery1707/testSpringRedirect/status/StatusRepository.java
@@ -1,21 +1,47 @@
 package name.valery1707.testSpringRedirect.status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.Enumeration;
+import java.util.jar.Manifest;
 
 @Repository
 public class StatusRepository {
+	private static final Logger LOG = LoggerFactory.getLogger(StatusRepository.class);
 
 	private ZonedDateTime startAt;
 	private String version;
 
 	@PostConstruct
-	public void init(){
+	public void init() {
 		startAt = ZonedDateTime.now();
-		version = "???";//todo Detect version
+		version = detectVersion();
+	}
+
+	private String detectVersion() {
+		Enumeration<URL> resources;
+		try {
+			resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+			while (resources.hasMoreElements()) {
+				URL url = resources.nextElement();
+				Manifest manifest = new Manifest(url.openStream());
+				String vendor = manifest.getMainAttributes().getValue("Implementation-Vendor");
+				if ("valery1707".equals(vendor)) {
+					return manifest.getMainAttributes().getValue("Implementation-Version");
+				}
+			}
+			return "Unknown: could not found manifest";
+		} catch (IOException e) {
+			LOG.error("Could not detect version", e);
+			return "Unknown: " + e.getMessage();
+		}
 	}
 
 	public Status getStatus() {
