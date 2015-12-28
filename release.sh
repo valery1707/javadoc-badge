9b17ba4dd517e47194b6543cd2c5f3e970904775
Valeriy.Vyrva <valery1707@gmail.com>
2015-12-28 16:08:03 +0600

    Script for making release

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+version_new=${1}
+
+version_cur=$(./gradlew properties | grep 'version:' | cut -d' ' -f2 | cut -d'-' -f1)
+
+update_version() {
+	echo "Update version from '${1}' to '${2}' in file ${3}"
+    sed -i.bak "s_version '${1}.*_version '${2}'_g" ${3}
+    rm ${3}.bak
+    chmod 777 ${3}
+}
+
+if [ "x${version_new}" = "x" ] ; then
+	version_1=$(echo -n "${version_cur}" | cut -d '.' -f1)
+	version_2=$(echo -n "${version_cur}" | cut -d '.' -f2)
+	version_3=$(echo -n "${version_cur}" | cut -d '.' -f3)
+	version_new=${version_1}.${version_2}.$[${version_3} + 1]
+fi
+
+echo "Updating project version: ${version_cur} -> ${version_new}"
+echo "New version is correct? (y/N)"
+read x
+if [ "$x" = "y" ] ; then
+	echo "Continue"
+elif [ "$x" = "Y" ] ; then
+	echo "Continue"
+else
+	echo "Stop"
+	exit 1
+fi
+
+update_version ${version_cur} ${version_new} build.gradle
+git add build.gradle
+git commit --quiet -m "Release version ${version_cur}"
+git tag -a -m "Release version ${version_cur}" v${version_cur}
+git push openshift master
+
+update_version ${version_new} ${version_new}-SNAPSHOT build.gradle
+git add build.gradle
+git commit --quiet -m "Start working on snapshot version ${version_new}"
+
+git push --quiet origin
+git push --tags --quiet origin
